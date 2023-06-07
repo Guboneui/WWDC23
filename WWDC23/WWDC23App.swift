b8080cf42aedcbd2ebf5ca13f95b5e764b89f77f
보니 <73548875+Guboneui@users.noreply.github.com>
2023-06-07 23:42:17 +0900

    first commit

---
+++ WWDC23/WWDC23App.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC23App.swift
+//  WWDC23
+//
+//  Created by 구본의 on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct WWDC23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
